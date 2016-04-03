435ba9ba8a161ec5c9b1776071fd8b7b6d4ca87b
johnshawstudent <john.shaw@mygeorgian.ca>
2016-04-02 23:47:42 -0400

    redirect fixes
    fixing redirect errors

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: 'Directory for brewerys',
-        message: 'Please login to edit your brewery information or Register to add your brewery to the directory'});
+      title: 'Directory for Brewerys',
+        message: 'Only registered users can edit the directory'});
 });
 
-router.get('/', function(req, res, next) {
-	res.render('publicview', {
-		title: 'Public view',
-		message: 'Find a brewery near you'});
-});
+
 // make this public so the rest of app can see it
 module.exports = router;
